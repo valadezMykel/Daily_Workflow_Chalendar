9f7580554814d3d75eaf560313a2dc9e976674d1
Mykel Valadez <valadezmykel@gmail.com>
2020-09-30 15:41:36 -0700

    I love it when a plan comes together

--- Assets/java.js
+++ Assets/java.js
@@ -1,5 +1,32 @@
 console.log($("#currentDay"))
 
 let today = $("#currentDay");
+let currentHour = moment().format("H");
 
 today.text(moment());
+
+// when the page loads it updates the css classes on the inputs based on the current hour
+$("input").each(function(){
+    let elTime = $(this).data("time");
+    let savedText = localStorage.getItem(elTime);
+    if(elTime == currentHour){
+        $(this).addClass("present");
+        $(this).val(savedText);
+    }
+    else if(elTime > currentHour){
+        $(this).addClass("future");
+        $(this).val(savedText);
+    }
+    else{
+        $(this).addClass("past");
+        $(this).val(savedText);
+    };
+});
+
+$(".row").on("click", function(event){
+    if(event.target.matches("button")){
+        let submitTextDiv = event.target.parentElement.children[1];
+        let timeKey = submitTextDiv.getAttribute("data-time");
+        localStorage.setItem(timeKey, submitTextDiv.value)
+    }
+})
